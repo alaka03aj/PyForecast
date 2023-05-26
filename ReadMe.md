69b1a7968deeb2387fd56a22bf15d7561f263ac0
Alaka A J <alaka03aj@gmail.com>
2023-05-26 13:14:44 +0530

    Updated ReadMe: Updated credits

--- ReadMe.md
+++ ReadMe.md
@@ -3,7 +3,7 @@
 ## Made for GitHub Copilot Hackathon
 
 ## Description
-This is a simple weather forecasting tool that uses the OpenWeatherMap API to retrieve weather data for a given city.
+This is a simple weather forecasting tool that uses the OpenWeatherMap API to retrieve weather data for a given city using Python. The tool displays current weather conditions, as well as the upcoming weather of the day in a 3 hour interval basis. 
 
 ## Tools Used
 ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
@@ -14,11 +14,22 @@ This is a simple weather forecasting tool that uses the OpenWeatherMap API to re
 ## Installation
 To install the tool, simply clone the repository and run the following command in the terminal:
 ```
-py weather.py
+# Warning: Make sure to have Python installed on your system.
+py weather.py <cityname>
+```
+
+Make sure to have the following libraries installed:
+- [Requests](https://docs.python-requests.org/en/master/)
+```
+pip install requests
+```
+or
+```
+py -m pip install requests
 ```
 
 ## Usage
-To use the tool, simply enter the name of the city you wish to retrieve weather data for. The tool will then display the current weather conditions, as well as a 5-day forecast. The tool will also display a graph of the temperature and humidity for the next 5 days.
+To use the tool, simply enter the name of the city you wish to retrieve weather data for. The tool will then display the current weather conditions, as well as the forecast for the rest of the day.
 ```
 py weather.py <cityname>
 ```
@@ -27,9 +38,13 @@ py weather.py <cityname>
 This tool can be used to retrieve weather data for any city in the world. It can be used to plan trips, or to simply check the weather in your area.
 
 ## Credits
-- The tool uses the [OpenWeatherMap API](https://openweathermap.org/api) to retrieve weather data for a given city.
+- [OpenWeatherMap API](https://openweathermap.org/api) to retrieve weather data for a given city.
 <!-- - The tool uses the [Matplotlib](https://matplotlib.org/) library to display a graph of the temperature and humidity for the next 5 days. -->
-- The tool uses the [Requests](https://docs.python-requests.org/en/master/) library to make HTTP requests to the OpenWeatherMap API.
+- [Requests](https://docs.python-requests.org/en/master/) library to make HTTP requests to the OpenWeatherMap API.
+- [JSON](https://docs.python.org/3/library/json.html) library to parse the JSON data retrieved from the OpenWeatherMap API.
+- [Datetime](https://docs.python.org/3/library/datetime.html) library to convert the UNIX timestamp to a readable date and time.
+- [Sys](https://docs.python.org/3/library/sys.html) library to retrieve the city name from the command line arguments.
+- [Github Copilot](https://copilot.github.com/) to help with the code.
 
 ## Team Name - Girl Boss
 ### Team Members
--- weather.py
+++ weather.py
@@ -2,6 +2,7 @@ import requests
 import constants
 import json
 import sys
+from datetime import datetime, timedelta
 
 weather_icons = {
     "01d": "☀️",
@@ -25,6 +26,8 @@ weather_icons = {
 }
 
 # Get weather data from openweathermap.org
+
+
 def get_weather(city):
     api_key = constants.API_KEY
     weather_url = "https://api.openweathermap.org/data/2.5/weather"
@@ -33,22 +36,64 @@ def get_weather(city):
         "appid": api_key,
         "units": "metric"
     }
-    
+
     try:
         response = requests.get(weather_url, params=params)
         data = json.loads(response.text)
+        # prints the current weather data
         print(f"Weather in {city}: {data['weather'][0]['description']}")
         print(f"{weather_icons[data['weather'][0]['icon']]}")
         print(f"Temperature: {data['main']['temp']}°C")
         print(f"Feels like: {data['main']['feels_like']}°C")
         print(f"Humidity: {data['main']['humidity']}%")
         print(f"Wind: {data['wind']['speed']} m/s")
+
+        # prints the forecast for the rest of the day
+        get_forecast(city, api_key)
+
     except requests.exceptions.ConnectionError:
         print("Something went wrong. Check your internet connection or try again later.")
     except requests.exceptions.Timeout:
-        print("Oh no! Seems like the server is taking too long to respond. Try again later.")
+        print(
+            "Oh no! Seems like the server is taking too long to respond. Try again later.")
     except:
         print("Seems like you entered wrong city name. Try again.")
+
+
+def get_forecast(city, api_key):
+    forecast_url = "https://api.openweathermap.org/data/2.5/forecast"
+    forecast_params = {
+        "q": city,
+        "appid": api_key,
+        "units": "metric"
+    }
+    try:
+        forecast_response = requests.get(forecast_url, params=forecast_params)
+        forecast_data = json.loads(forecast_response.text)
+        forecast_temperatures = []
+        forecast_humidity = []
+        forecast_dates = []
+        forecast_icons = []
+        now = datetime.now()
+        for forecast in forecast_data['list']:
+            forecast_date = datetime.fromtimestamp(forecast['dt'])
+            if forecast_date.date() == now.date() and forecast_date >= now:
+                forecast_dates.append(forecast_date)
+                forecast_temperatures.append(forecast['main']['temp'])
+                forecast_humidity.append(forecast['main']['humidity'])
+                forecast_icons.append(forecast['weather'][0]['icon'])
+
+        print("\nWeather Forecast for the rest of the day:")
+        for i in range(len(forecast_dates)):
+            forecast_time = forecast_dates[i].strftime("%H:%M")
+            temperature = forecast_temperatures[i]
+            humidity = forecast_humidity[i]
+            weather_icon = weather_icons.get(forecast_icons[i], "")
+            print(
+                f"{forecast_time} - {weather_icon} Temperature: {temperature}°C, Humidity: {humidity}%")
+    except:
+        print("Something went wrong. Try again.")
+
 
 def main():
     # print(sys.argv) - prints the list of arguments passed to the script
@@ -58,6 +103,7 @@ def main():
 
     city = sys.argv[1]
     get_weather(city)
+
 
 if __name__ == "__main__":
     main()
